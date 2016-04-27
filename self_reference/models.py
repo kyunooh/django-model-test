99ef48b020e3766853141373b7c8858a0dac4911
Hyeon-mook Jerry Choi <chm073@gmail.com>
2016-04-27 22:50:30 +0900

    add gitignore and update models

--- self_reference/models.py
+++ self_reference/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class SelfReferenceModel(models.Model):
-    object_id = models.AutoField()
+    object_id = models.AutoField(primary_key=True)
     parent_id = models.ForeignKey('self')
     
