72ab28befb1e5095082a1d753598a6735a0704d2
Hyeon-mook Jerry Choi <chm073@gmail.com>
2016-04-27 22:37:09 +0900

    add app

--- model_test/model_test/settings.py
+++ model_test/model_test/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'self_reference',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,10 +78,14 @@ WSGI_APPLICATION = 'model_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'model_test',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
     }
 }
+
 
 
 # Password validation
---
+++ model_test/self_reference/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SelfReferenceConfig(AppConfig):
+    name = 'self_reference'
