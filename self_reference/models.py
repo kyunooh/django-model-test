5456ddcb0cf804df985a00c943ac05d41fcf10fc
Hyeon-mook Jerry Choi <chm073@gmail.com>
2016-04-28 22:19:09 +0900

    add forien model

--- self_reference/models.py
+++ self_reference/models.py
@@ -4,4 +4,8 @@ from django.db import models
 class SelfReferenceModel(models.Model):
     object_id = models.AutoField(primary_key=True)
     parent_id = models.ForeignKey('self')
-    
+
+
+class ForeignModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    foreign_id = models.ForeignKey(SelfReferenceModel)
