a67f626e699f46f97fa8ea7bdb448e14a5538128
ChoiHyeonMook <chm073@gmail.com>
2016-05-05 19:14:49 +0900

    add selenium test

--- self_reference/tests.py
+++ self_reference/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test.testcases import LiveServerTestCase
+from selenium import webdriver
 
-# Create your tests here.
+
+class TravisTestCase(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+        
+    def test_title_is_selenium_test(self):
+        self.browser.get(self.live_server_url)
+        self.assertEquals("selenium test", self.browser.title)
